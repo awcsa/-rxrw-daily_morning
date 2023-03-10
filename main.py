54f7228bd6880e6abaf8f316c8d5639558a2f4d3
awcsa <127496479+awcsa@users.noreply.github.com>
2023-03-10 19:01:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,47 +7,82 @@ import os
 import random
 
 today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+start_date = "2022-03-21"
+city = "周口"
+birthday = "04-24"
+birthday2 = "04-26"
+menses = "01-25"
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+app_id = "wxa4ff49d46025aed2"
+app_secret = "55acc66a2b8c7af882f4288e13777b47"
 
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
-
+user_id = "oSWon6NeGCoSsnyyG_zHBU_onuag,oSWon6ILtTDeCUElNWaHymMqAtV8"
+template_id = "oSBhhnVqR2hWjGYculaCkssYPQ4T0K8r6oirupx9GGA"
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
+    res = requests.get(url).json()
+    wet = res['data'][0]
+    return wet['tem'], wet['tem1'], wet['tem2'], wet['phrase'], wet['humidity']
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
 
+def get_birthday2():
+    next = datetime.strptime(str(date.today().year) + "-" + birthday2, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
+# def get_menses():
+#     next = datetime.strptime(str(date.today().year) + "-" + menses, "%Y-%m-%d")
+#     if next < datetime.now():
+#         next = next.replace(year=next.year + 1)
+#     return (next - today).days
+def get_menses():
+    delta = today - datetime.strptime(menses, "%Y-%m-%d")
+    return delta.days
+
+def get_curmenses():
+    return menses
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+# cur max min
+cur, maxs, mins, weather, humidity = get_weather()
+data = {"weather": {"value": weather, "color": get_random_color()},
+        "cur": {"value": cur, "color": get_random_color()},
+        "humidity": {"value": humidity, "color": get_random_color()},
+        "menses": {"value": get_menses(), "color": get_random_color()},
+        "premenses": {"value": get_curmenses(), "color": get_random_color()},
+        "maxs": {"value": maxs, "color": get_random_color()},
+        "mins": {"value": mins, "color": get_random_color()},
+        "love_days": {"value": get_count(), "color": get_random_color()},
+        "birthday_left": {"value": get_birthday(), "color": get_random_color()},
+        # "birthday_left2": {"value": get_birthday2(), "color": get_random_color()},
+        "words": {"value": get_words(), "color": get_random_color()}
+        }
+everyone = user_id.split(",")
+for i in everyone:
+    res = wm.send_template(i, template_id, data)
+    print(res)
+
